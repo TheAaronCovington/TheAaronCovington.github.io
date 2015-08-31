8e324e083cbf413db4f5cdf31015160c607b6027
Aaron Covington <TheAaronCovington@gmail.com>
2015-08-31 18:21:26 -0500

    Create NavScroll.js

---
+++ js/NavScroll.js
@@ -0,0 +1,20 @@
+//Provides smooth scrolling animation
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+//When selection is made on responsive mobile navbar this will close it automatically
+$('.navbar-collapse a').click(function(){
+    $('.navbar-collapse').collapse('hide');
+});
