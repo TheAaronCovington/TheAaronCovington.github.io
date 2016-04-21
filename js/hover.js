5ddc7c4bb3dd8e02e8bac9956fb3f918b1b8aff4
Aaron Covington <TheAaronCovington@gmail.com>
2016-04-21 02:37:19 -0500

    Create hover.js

---
+++ js/hover.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	$('#span1').hover(function(){
+		$("#img1").css('opacity', '0.2');	
+	}, function(){
+		$("#img1").css('opacity', '1');
+	});
+	$('#img1').hover(function(){
+		$(this).css('opacity', '0.2');	
+	}, function(){
+		$(this).css('opacity', '1');
+	});
+	$('#span2').hover(function(){
+		$("#img2").css('opacity', '0.2');	
+	}, function(){
+		$("#img2").css('opacity', '1');
+	});
+	$('#img2').hover(function(){
+		$(this).css('opacity', '0.2');	
+	}, function(){
+		$(this).css('opacity', '1');
+	});
+	$('#span3').hover(function(){
+		$("#img3").css('opacity', '0.2');	
+	}, function(){
+		$("#img3").css('opacity', '1');
+	});
+	$('#img3').hover(function(){
+		$(this).css('opacity', '0.2');	
+	}, function(){
+		$(this).css('opacity', '1');
+	});
+	$('#span4').hover(function(){
+		$("#img4").css('opacity', '0.2');	
+	}, function(){
+		$("#img4").css('opacity', '1');
+	});
+	$('#img4').hover(function(){
+		$(this).css('opacity', '0.2');	
+	}, function(){
+		$(this).css('opacity', '1');
+	});
+	$('#span5').hover(function(){
+		$("#img5").css('opacity', '0.2');	
+	}, function(){
+		$("#img5").css('opacity', '1');
+	});
+	$('#img5').hover(function(){
+		$(this).css('opacity', '0.2');	
+	}, function(){
+		$(this).css('opacity', '1');
+	});
+	$('#span6').hover(function(){
+		$("#img6").css('opacity', '0.2');	
+	}, function(){
+		$("#img6").css('opacity', '1');
+	});
+	$('#img6').hover(function(){
+		$(this).css('opacity', '0.2');	
+	}, function(){
+		$(this).css('opacity', '1');
+	});
+});
