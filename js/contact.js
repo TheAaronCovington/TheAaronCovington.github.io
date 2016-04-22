f6d9913d1c36912534305bfe5038b710909b839d
TheAaronCovington <TheAaronCovington@gmail.com>
2016-04-22 17:14:28 -0500

    Contact Form Updates

---
+++ js/contact.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.btn-success').click(function(){
+		var isValid = true;
+	  $('.form-control').each(function() {
+	    if ( $(this).val() === '' )
+	        isValid = false;
+	    	if($(this).is("#form_phone")){
+	    		isValid = true;
+	    	}
+	  });
+	  if(isValid){
+	  	alert("Email has been sent successfully!");
+	  }
+	});
+});
+
+function validateForm() {
+
+}
