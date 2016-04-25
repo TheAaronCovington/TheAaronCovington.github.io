d7ad224883b818c57f11e1c1d72c918eed6541ec
Aaron Covington <TheAaronCovington@gmail.com>
2016-04-25 02:21:18 -0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$('.btn-success').click(function(){
 		var isValid = true;
 	  $('.form-control').each(function() {
-	    if ( $(this).val() === '' || $(this).val() === 'Firstname *' || $(this).val() === 'Lastname *' || $(this).val() === 'Email *' || $(this).val() === 'Message for me *'){
+	    if ( $(this).val() === '' || $(this).val() === 'Firstname *' || $(this).val() === 'Lastname *' || $(this).val() === 'Email *' || $(this).val() === 'Message for me *')
 	        isValid = false;
 	    	if($(this).is("#form_phone")){
 	    		isValid = true;
